da00d3a7b1a21d38e64516659e0f41914a9f1f2e
Simon Zeni <simonzeni@gmail.com>
2018-12-23 01:28:00 -0400

    trunkston : use wlt_backend

--- trunkston/main.c
+++ trunkston/main.c
@@ -1,9 +1,13 @@
 #include "wltrunk/server.h"
+#include "wltrunk/backend.h"
 
 int main(int argc, char **argv)
 {
 	struct wlt_server *server = create_wlt_server();
 	assert(server);
+
+	struct wlt_backend *backend = create_wlt_backend(server);
+	assert(backend);
 
 	return 0;
 }
